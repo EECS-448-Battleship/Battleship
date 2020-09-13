192da97ccd74a59bf29803e6a1ccde50d3415ee2
Thomas Atkins <dev.csnova@gmail.com>
2020-09-13 15:28:26 -0500

    Made the board a class

--- battleship/board.py
+++ battleship/board.py
@@ -1,24 +1,25 @@
 class board:
 
-#creating 9 by 9 board of O's
-def board():
-    board = []
-    for i in range(0, 9):
-        board.append(["O"] * 9)
+    #creating 9 by 9 board of O's
+    def __init__(self):
+        self.board = []
+        for i in range(0, 9):
+            self.board.append(["O"] * 9)
 
-#printing the board with spaces in between
-def print_board(board):
-    for i in board:
-        print " ".join(i)
+    # # #printing the board with spaces in between
+    # def print_board(board):
+    #     for row in board:
+        
 
-#setup function that places the ships and places them on the board
-def setUp():
-    for i in range(0, 9)
-        if (set_ships(i) == true)
-            board.append(["S"])
 
-#update function
-def update():
-    for i in range(0, 9):
-        if (hit(i) == true)
-            board.append(["X"])
+    #setup function that places the ships and places them on the board
+    def setUp():
+        for i in range(0, 9):
+            if (set_ships(i) == true):
+                board.append(["S"])
+
+    #update function
+    def update():
+        for i in range(0, 9):
+            if (hit(i) == true):
+                board.append(["X"])
