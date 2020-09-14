979c825a0d9b41f2ba0bbecf7f46b19b912969cc
Thomas Atkins <dev.csnova@gmail.com>
2020-09-13 23:00:40 -0500

    fixed doc script

--- make_doc.bat
+++ make_doc.bat
@@ -1,6 +1,1 @@
-pdoc --html --output-dir doc .\battleship\board.py --force
-pdoc --html --output-dir doc .\battleship\game.py --force
-pdoc --html --output-dir doc .\battleship\Player.py --force
-pdoc --html --output-dir doc .\battleship\Ship.py --force
-pdoc --html --output-dir doc .\battleship\Terminal.py --force
-
+pdoc --html --output-dir doc battleship --force
