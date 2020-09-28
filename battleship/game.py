c5f2966385d4cd942e3559fe8530762ddfaa3954
garrettmills <shout@garrettmills.dev>
2020-09-28 11:27:52 -0500

    Clean up game class

--- battleship/game.py
+++ battleship/game.py
@@ -1,7 +1,6 @@
-from .Ship import Ship
 from .Player import Player
-from .board import board
 from .Terminal import Terminal
+
 
 class Game:
 	""" The class 
@@ -9,95 +8,83 @@ class Game:
 	Attributes:
 		p1: The first player object.
 		p2: The second player object.
-		aTerminal: The terminal helper object.
-
-
+		terminal: The terminal helper object.
 	"""
-	aTerminal = Terminal()
+
+	terminal = Terminal()
 
 	def __init__(self):
 		"""
 		Initializes the game
-
 		"""
-		self.aTerminal.printWelcome()
+
+		self.terminal.printWelcome()
 		
 		player_one_name = input("Player 1, please enter your name: ")
 		self.p1 = Player(player_one_name)
 		self.p1.set_ships()
-		self.aTerminal.printSelfBoard(self.p1.board)
+		self.terminal.printSelfBoard(self.p1.board)
 
 		input("Press enter to Contitue")
-		self.aTerminal.clearScreen()
+		self.terminal.clearScreen()
 
 		player_two_name = input("Player 2, please enter your name: ")
 		self.p2 = Player(player_two_name)
 		self.p2.set_ships()
-		self.aTerminal.printSelfBoard(self.p2.board)
-
+		self.terminal.printSelfBoard(self.p2.board)
 
 		self.p1.other_player = self.p2
 		self.p2.other_player = self.p1
 
-
 		input("Press enter to begin")
-		self.aTerminal.clearScreen()
+		self.terminal.clearScreen()
 
-
-		
-	#begins the game & conducts turn untill winner determined
 	def play_game(self):
 		"""
-
-		Startes the game 
-
+		Starts the game
 		"""
 		won_game = False
 	
 		while(won_game == False):
-			
-			
-				
-			#board of player one is shown and chooses target area to hit 
-			self.aTerminal.playerView(self.p1,self.p2)
-			#self.player_one_board.print_board(self.p1)
+			# board of player one is shown and chooses target area to hit
+			self.terminal.playerView(self.p1,self.p2)
+			# self.player_one_board.print_board(self.p1)
 			
 			hit = self.p1.fire()
 			
 			if hit:
-				self.aTerminal.printHit()
+				self.terminal.printHit()
 			else:
-				self.aTerminal.printMiss()
+				self.terminal.printMiss()
 			
 			input()
 
-			#checks if player's boats are floating
-			if(self.p2.board.any_left()==False):
-				won_game==True
-				self.aTerminal.printWinner(self.p1,self.p2)
+			# checks if player's boats are floating
+			if not self.p2.board.any_left():
+				won_game = True
+				self.terminal.printWinner(self.p1,self.p2)
 				break
 			
-			if won_game==False:	
-				#players switch
-				self.aTerminal.printSwitchPrompt(self.p2)
+			if not won_game:
+				# players switch
+				self.terminal.printSwitchPrompt(self.p2)
 				
-				
-				#board of player two is shown and chooses target area to hit
-				self.aTerminal.playerView(self.p2,self.p1)
-				#player_two_board.print_board()
+				# board of player two is shown and chooses target area to hit
+				self.terminal.playerView(self.p2,self.p1)
+				# player_two_board.print_board()
 				hit = self.p2.fire()
 
 				if hit:
-					self.aTerminal.printHit()
+					self.terminal.printHit()
 				else:
-					self.aTerminal.printMiss()
+					self.terminal.printMiss()
 				
 				input()
 				
-				#checks if player's boats are floating
-				if(self.p1.board.any_left()==False):
-					won_game==True
-					self.aTerminal.printWinner(self.p2,self.p1)
-					break			
-				self.aTerminal.printSwitchPrompt(self.p1)	
-	
+				# checks if player's boats are floating
+				if not self.p1.board.any_left():
+					won_game = True
+					self.terminal.printWinner(self.p2,self.p1)
+					break
+
+				self.terminal.printSwitchPrompt(self.p1)	
