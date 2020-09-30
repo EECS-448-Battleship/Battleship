f53ecb4e7b9d32383aca671329b6cdc1cf70d023
javier-barea <javierbarealara@gmail.com>
2020-09-29 20:23:57 -0500

    Created interface file

---
+++ battleship/interface.py
@@ -0,0 +1,17 @@
+import pygame
+import random
+import math
+
+# initialize pygame
+pygame.init()
+
+# create a screen
+screen = pygame.display.set_mode((800, 600))
+
+pygame.display.set_caption("BattleShip")
+
+
+running = True
+
+while running:
+    print('Hello there')
