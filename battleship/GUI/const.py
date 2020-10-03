303078dda21baeaa8e27bed6e55d28a6d430dd1f
garrettmills <shout@garrettmills.dev>
2020-10-02 22:56:53 -0500

    Add pygame requirement and GUI constants (#3)

---
+++ battleship/GUI/const.py
@@ -0,0 +1,19 @@
+# This is a file for keeping track of various constants used in the GUI
+
+import os
+os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
+import pygame as pg
+from pygame.locals import *
+
+pg.init()
+
+pygame = pg
+width = 1000
+height = 600
+fps = 30
+
+fill_color = (66, 123, 181)
+text_color = (255, 255, 255)
+text_bkg_color = (29, 58, 87)
+
+clock = pygame.time.Clock()
