74839e0d2eb07986c0916f48b39236322bd2acc2
garrettmills <shout@garrettmills.dev>
2020-10-02 20:55:41 -0500

    Add stub methods for get_fire_coordinates_* (#2)

--- battleship/AIPlayer.py
+++ battleship/AIPlayer.py
@@ -21,3 +21,23 @@ class AIPlayer(Player):
         cols = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I']
         rows = [1, 2, 3, 4, 5, 6, 7, 8, 9]
         return str(random.choice(cols)) + str(random.choice(rows))
+
+    def get_fire_coordinates_easy(self):
+        # TODO implement this
+        return 'A1'
+
+    def get_fire_coordinates_medium(self):
+        # TODO implement this
+        return 'A1'
+
+    def get_fire_coordinates_hard(self):
+        # TODO implement this
+        return 'A1'
+
+    def get_fire_coordinates(self):
+        if self.ai_difficulty == AIDifficulty.Easy:
+            return self.get_fire_coordinates_easy()
+        elif self.ai_difficulty == AIDifficulty.Medium:
+            return self.get_fire_coordinates_medium()
+        else:
+            return self.get_fire_coordinates_hard()
